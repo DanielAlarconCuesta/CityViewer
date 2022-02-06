7fb47e78b5275b1b751a07458f0b73691ae87708
Daniel1945 <danielalarconcuesta@gmail.com>
2022-02-06 18:28:36 +0100

    hook useFetchCities developed

---
+++ frontend/src/hooks/useFetchCities.tsx
@@ -0,0 +1,40 @@
+import { useState, useEffect, useCallback } from "react";
+
+import CityService from "../services/CityService/CityService";
+
+import CityQuery from "../types/CityQuery";
+import City from "../types/City";
+
+function useFetchCities(cityQuery: CityQuery, cities: City[], setCities: any): any {
+
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
+
+  const sendQuery = useCallback(async () => {
+
+    await setLoading(true);
+    await setError(false);
+    
+    await CityService.getCities(cityQuery).then((newCities: City[]) => {
+
+        if (newCities.length > 0) {
+            setCities([...cities, ...newCities])
+            setHasMore(true);
+        } else {
+            setHasMore(false);
+        }
+    })
+
+    setLoading(false);
+
+  }, [cityQuery]);
+
+  useEffect(() => {
+    sendQuery();
+  }, [cityQuery, sendQuery]);
+
+  return { loading, error, hasMore };
+}
+
+export default useFetchCities;
